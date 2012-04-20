048c288ef7ff520eef58d3d14a700d5110eba67a
Justin Gray <Justin.S.Gray@gmail.com>
2012-04-20 15:52:21 -0400

    initial bootstrap implementation

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 from paddock.models import Club
 
@@ -8,4 +9,6 @@ def clubs(request):
     
     context = {'clubs':clubs,
                'club_count':len(clubs)}
-    return render_to_response('clubs.html',context)
+    return render_to_response('clubs.html',
+                              context,
+                              context_instance=RequestContext(request))
