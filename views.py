4258419e8c4943b99bc8d50790941471b652fb45
Justin Gray <Justin.S.Gray@gmail.com>
2012-04-21 13:19:30 -0400

    initial user login form

--- views.py
+++ views.py
@@ -1,10 +1,16 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.core.urlresolvers import reverse
+
+
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 from paddock.models import Club
 
 def clubs(request):
-
+    """club index page"""
     clubs = Club.objects.all()
     
     context = {'clubs':clubs,
@@ -12,3 +18,25 @@ def clubs(request):
     return render_to_response('paddock/clubs.html',
                               context,
                               context_instance=RequestContext(request))
+
+def new_user(request): 
+    """handles redering of new user form and creation of users"""
+    
+    #TODO: redirect somewhere else if authenticated user
+    
+    if request.method == 'POST': # If the form has been submitted...
+        form = UserCreationForm(request.POST) #bound for, with submitted data
+        if form.is_valid(): 
+            #then create the user from the form data
+            u = User.objects.create_user(form.cleaned_data['username'], 
+                                         password=form.cleaned_data['password1'])
+            u.save()
+            return HttpResponseRedirect(reverse('paddock.views.clubs'))
+        else: 
+            #how to redirect bact to new_user page with errors? 
+            pass
+    else: 
+        form = UserCreationForm() #unbound form (no data)        
+        return render_to_response('paddock/new_user.html',
+                                  {'form':form},
+                                  context_instance=RequestContext(request))
