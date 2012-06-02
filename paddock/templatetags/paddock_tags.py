231a465b0ed863abe3eefbe3cdf51bb483e79a4f
Justin Gray <Justin.S.Gray@gmail.com>
2012-06-01 22:59:05 -0400

    started DataTables integration

--- paddock/templatetags/paddock_tags.py
+++ paddock/templatetags/paddock_tags.py
@@ -1,3 +1,4 @@
+import os
 from django import template
 
 register = template.Library()
@@ -5,4 +6,16 @@ register = template.Library()
 @register.inclusion_tag('paddock/events_table.html')
 def events_table(events): 
     return {'events': events}
+
+
+@register.inclusion_tag('paddock/direction_bubble.html')
+def directions_bubble(lat,long,name): 
+    return {'lat': lat,
+            'long': long,
+            'name': name}
+
+@register.inclusion_tag('paddock/reg_list.html')
+def reg_table(regs,table_id=None):
+    return {'table_id':table_id,
+            'regs':regs}
     
