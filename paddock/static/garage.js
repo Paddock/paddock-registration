e867850cc4739c484e6645a56bda777df3d9b671
Justin Gray <Justin.S.Gray@gmail.com>
2012-08-04 18:08:08 -0400

    started to try bootstrap

--- paddock/static/garage.js
+++ paddock/static/garage.js
@@ -1,60 +1,36 @@
-Garage = Ember.Application.create({
-    ready: function(){
-        // Call the superclass's `ready` method.
-        user_view.appendTo('#user_info')
-        carsView.appendTo('#user_cars')
-        this._super();
+(function($){
+
+  var Car = Backbone.Model.extend({
+    defaults:{
+      name:"The Red Sea",  
+      desc:"1990 Mazda Miata"
+    },
+  });
+  
+  var User = Backbone.Model.extend({
+  
+      defaults: {
+          first_name:"Justin",
+          last_name:"Gray",
+          email:"justin.s.gray@gmail.com"
+      },
+      
+  });
+  
+  var UserView = Backbone.View.extend({
+    el: $("#user_info"),
+    model: new User,
+    initialize: function(){
+        var source = $("#user_view").html();
+        this.template = Handlebars.compile(source);  
+        this.render();
+    },
+    render: function(){
+      var user = this.model.toJSON();
+      $(this.el).append(this.template(user));
+      return this;  
     }
-});
+  });
 
-/**************************
-* Models
-**************************/
-
-Garage.car = Ember.Object.extend({
-    name:"The Red Sea",
-    color:"Red",
-    year:"1990", 
-    make:"Mazda",
-    model:"Miata",
-    avatar:"/media/car_avatars/justingray_Miata_avatar",
-    thumb:"/media/car_thumbs/justingray_Miata_avatar",
-});
-var car1 = Garage.car.create({
-    name:"The Red Sea",
-    color:"Red",
-    year:"1990", 
-    make:"Mazda",
-    model:"Miata",
-    avatar:"/media/car_avatars/justingray_Miata_avatar",
-    thumb:"/media/car_thumbs/justingray_Miata_avatar",
-});
-/**************************
-* Controlers
-**************************/
-
-Garage.carsController = Ember.ArrayController.create({
-   content: [car1,car1],
-});
-
-/**************************
-* Views
-**************************/
-
-var user_view = Ember.View.create({
-  templateName: 'user_info',
-  first_name: "Justin",
-  last_name: "Gray",
-  email: "Justin.S.Gray@gmail.com",
-  password: "xxxxxxx"
-});
-
-carsView = Ember.CollectionView.create({
-  content: Garage.carController.content,
-  emptyView: Ember.View.extend({
-      template: Ember.Handlebars.compile("The collection is empty")
-    }),
-  itemViewClass: Ember.View.extend({
-      template: Ember.Handlebars.compile("Testing:{{view.content.name}}")
-    }),
-})
+  var user_view = new UserView;
+})(jQuery);
