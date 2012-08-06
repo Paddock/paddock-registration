d67d44ec58fc68fd4523a0194f9849a0959aba6e
Justin Gray <Justin.S.Gray@gmail.com>
2012-08-05 22:20:06 -0400

    backbone coupons table done

--- paddock/static/garage.js
+++ paddock/static/garage.js
@@ -1,12 +1,8 @@
 (function($){
 
-  var Car = Backbone.Model.extend({
-    defaults:{
-      name:"The Red Sea",  
-      desc:"1990 Mazda Miata"
-    },
-  });
-  
+  //////////////////////////////////
+  // Models
+  //////////////////////////////////
   var User = Backbone.Model.extend({
   
       defaults: {
@@ -16,6 +12,35 @@
       },
       
   });
+  
+
+  var Car = Backbone.Model.extend({
+    defaults:{
+      name:"The Red Sea",  
+      desc:"1990 Mazda Miata"
+    },
+  });
+  
+  var Cars = Backbone.Collection.extend({
+    model: Car,
+  });
+  
+  var Coupon = Backbone.Model.extend({
+    defaults:{
+      code:"xxxx",
+      value:"0.00",
+      expiration:"N/A",
+      uses:"N/A",
+    },
+  });
+  
+  var Coupons = Backbone.Collection.extend({
+    model: Coupon,  
+  });
+  
+  //////////////////////////////////
+  // Views
+  //////////////////////////////////
   
   var UserView = Backbone.View.extend({
     el: $("#user_info"),
@@ -31,6 +56,46 @@
       return this;  
     }
   });
-
+  
+  var TableCollectionView  = Backbone.View.extend({
+    template: Handlebars.compile("<tr>{{{row}}}</tr>"),
+    render: function() {
+      var that = this
+      _.each(this.collection.models,function(item){
+        var columns = that.row_template(item.toJSON());
+        $(that.el).append(that.template({'row':columns}));      
+      });    
+    },
+  });
+  
+  CarsView = TableCollectionView.extend({
+    el: $("#cars_table"),
+    row_template: Handlebars.compile("<td>{{name}}</td><td>{{desc}}</td>"),
+    
+    initialize: function() {
+      var c = new Car;
+      this.collection = new Cars([c,c,c]);
+      this.render();
+    }
+  });
+  
+  CouponsView = TableCollectionView.extend({
+    el: $('#coupons_table'),
+    row_template: Handlebars.compile("<td>{{code}}</td><td>{{value}}</td><td>{{expiration}}</td><td>{{uses}}</td>"),
+    initialize: function() {
+      var c = new Coupon;
+      this.collection = new Coupons([c,c,c]);
+      this.render();
+    }
+  });
+  
+  
+  
+  //////////////////////////////////
+  // App Setup
+  //////////////////////////////////
   var user_view = new UserView;
+  var cars_view = new CarsView;
+  var coupons_view = new CouponsView;
+  
 })(jQuery);
