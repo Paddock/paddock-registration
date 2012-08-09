d116a70bf3ddcfa3e5520db9afa2de6eeca5d89e
Justin Gray <Justin.S.Gray@gmail.com>
2012-08-08 20:58:29 -0400

    a bit of good view restructuring

--- paddock/static/garage.js
+++ paddock/static/garage.js
@@ -72,7 +72,6 @@
   });
   
   var CollectionView  = Backbone.View.extend({
-    collection:null,
     template: Handlebars.compile("<tr>{{{row}}}</tr>"),
     render: function() {
       var that = this;
@@ -104,35 +103,60 @@
     template: Handlebars.compile("<li>{{{row}}}</li>"),
   });
   
-  var CarsView = TableCollectionView.extend({
-    el: $("#cars_table"),
-    cols:{"name":"Name",
-          "desc":"Car",
-          "":null
-         },
-
-    events: {
-      'click .btn': function(){alert("testing");},
-    } ,    
+  var CarView = Backbone.View.extend({
+    tagName:"tr",
     row_template: Handlebars.compile('<td><i class="icon-picture avatar_pic" \
-            data-title="{{ name }}" href="{{ thumb }}"></i> \
-            {{name}}</td><td>{{desc}}</td> \
-            <td><i class="icon-edit"></i><i class="icon-trash"></i></td>'),
-    
+          data-title="{{ name }}" href="{{ thumb }}"></i> \
+          {{name}}</td><td>{{desc}}</td> \
+          <td><i class="icon-edit"></i><i class="icon-trash"></i></td>'),     
+    render: function(){
+      var row = this.row_template(this.model.toJSON());
+      this.$el.empty();
+      this.$el.append(row);
+      this.$('.avatar_pic').avatar_popover();
+      return this;
+    },
+  });
+
+  var CarsView = TableCollectionView.extend({
+    //el: $("#cars_table"),
+    events: {
+      'click .btn': 'add_car',
+    } ,        
     initialize: function() {
       var c = new Car;
-      this.collection = new Cars([c,c,c]);
+      var that = this;
+      this.collection = new Cars([c]);
+      this._carviews = [];
+      this.collection.each(function(car){
+        that._carviews.push(new CarView({'model':car}));
+      });
+
+      _(this).bindAll('render');
+      this.collection.bind('add', this.render);
       this.render();
     },
 
     render: function(){
       var that = this;
-      CarsView.__super__.render.apply(that);
-
-      $(that.el).append('<tr><td><input type="text" style="width:90%" name="name"></td> \
+      //var el = $(that.el);
+      this.$el.empty();
+      _(this._carviews).each(function(cv){
+        that.$el.append(cv.render().$el);
+      });
+      console.log(this.$el.html());
+      this.$el.append('<tr><td><input type="text" style="width:90%" name="name"></td> \
         <td><input type="text" style="width:90%" name="desc"></td> \
         <td><button class="btn btn-primary">Add Car</button></td> \
         </tr>')
+      return this;
+    },
+
+    add_car: function(){
+      var name = this.$('input[name="name"]').val();
+      var desc = this.$('input[name="desc"]').val();
+      var c = new Car({'name':name, 'desc':desc})
+      this.collection.push(c);
     },
   });
   
@@ -153,7 +177,6 @@
       var c = new Coupon;
       this.collection = new Coupons([c,c,c]);
       this.render();
-      $(".avatar_pic").avatar_popover();
     }
   });
 
@@ -171,7 +194,7 @@
   // App Setup
   //////////////////////////////////
   var user_view = new UserView;
-  var cars_view = new CarsView;
+  var cars_view = new CarsView({'el':$('#cars_table')});
   var coupons_view = new CouponsView;
   var events_view = new EventsView;
   
